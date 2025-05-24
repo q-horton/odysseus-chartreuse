b9b73da435434e070c41b1c4a7d742d94fadd7d1
yogabhai <yoganshbhalla@gmail.com>
2025-05-24 18:23:52 +1000

    Support up to 1650 bytes via extended advertising.

--- nodes/test/sensornode_ble_peripheral/peripheral_simple_ext_adv/src/main.c
+++ nodes/test/sensornode_ble_peripheral/peripheral_simple_ext_adv/src/main.c
@@ -4,24 +4,28 @@
 #include <zephyr/bluetooth/hci.h>
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gap.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #define DEVICE_NAME "SensorNode"
 #define SENSOR_ADV_INTERVAL_MS 1000
 
 // Custom sensor data payload, length + 5 bytes < 255
-static uint8_t adv_payload[240];  
+__aligned(4) static uint8_t adv_payload[230] = {0};  
 // To use max of 1650, define multuple adv_payloads, and add in ad[] below (TBD)
-//static uint8_t adv_payload_2[240]; // For future use
-//static uint8_t adv_payload_3[240]; // For future use
+__aligned(4) static uint8_t adv_payload_two[230] = {0}; // For future use
+__aligned(4) static uint8_t adv_payload_3[230] = {0}; // For future use
 //static uint8_t adv_payload_4[240]; // For future use
 //static uint8_t adv_payload_5[240]; // For future use
 //static uint8_t adv_payload_6[240]; // For future use
 
 static struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, sizeof(DEVICE_NAME) - 1),
     BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload, sizeof(adv_payload)),
-    // BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload_2, sizeof(adv_payload_2)),
-    // BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload_3, sizeof(adv_payload_3)),
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload_two, sizeof(adv_payload_two)),
+    BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload_3, sizeof(adv_payload_3)),
     // BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload_4, sizeof(adv_payload_4)),
     // BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload_5, sizeof(adv_payload_5)),
     // BT_DATA(BT_DATA_MANUFACTURER_DATA, adv_payload_6, sizeof(adv_payload_6)),
@@ -43,7 +47,12 @@ void update_adv_payload(void) {
 
     // Remaining bytes can remain zero
     for (int i = 5; i < sizeof(adv_payload); i++) {
-        adv_payload[i] = i % 2;
+        adv_payload[i] = 0xFF;
+    }
+
+    // // fill adv_payload_two with some data for future use
+    for (int i = 0; i < sizeof(adv_payload_two); i++) {
+        adv_payload_two[i] = counter; // Example data
     }
 
     // print the payload for debugging
@@ -51,18 +60,27 @@ void update_adv_payload(void) {
     for (int i = 0; i < sizeof(adv_payload); i++) {
         printk("%02X ", adv_payload[i]);
     }
+    for (int i = 0; i < sizeof(adv_payload_two); i++) {
+        printk("%02X ", adv_payload_two[i]);
+    }
     printk("\n");
 }
 
 void main(void) {
     int err;
-    struct bt_le_adv_param adv_params = {
-        .options = BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_USE_IDENTITY,
-        .interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
-        .interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
-        .id = BT_ID_DEFAULT,
-        .sid = 0,
-    };
+    // struct bt_le_adv_param adv_params = {
+    //     .options = BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_USE_IDENTITY,
+    //     .interval_min = BT_GAP_ADV_FAST_INT_MIN_2,
+    //     .interval_max = BT_GAP_ADV_FAST_INT_MAX_2,
+    //     .id = BT_ID_DEFAULT,
+    //     .sid = 0,
+    // };
+
+    struct bt_le_adv_param adv_params = BT_LE_ADV_PARAM_INIT(
+        BT_LE_ADV_OPT_EXT_ADV | BT_LE_ADV_OPT_USE_IDENTITY,
+        BT_GAP_ADV_FAST_INT_MIN_2,
+        BT_GAP_ADV_FAST_INT_MAX_2,
+        NULL);
 
     printk("Starting SensorNode BLE extended advertiser\n");
 
@@ -77,7 +95,6 @@ void main(void) {
     update_adv_payload();
 
     err = bt_le_ext_adv_create(&adv_params, NULL, &adv_set);
-    //err = bt_le_ext_adv_create(BT_LE_EXT_ADV_CONN, NULL, &adv_set);
     if (err) {
         printk("Failed to create extended advertiser (err %d)\n", err);
         return;
@@ -100,7 +117,19 @@ void main(void) {
     while (1) {
         printk("Updating advertisement data...\n");
         update_adv_payload();
-        bt_le_ext_adv_set_data(adv_set, ad, ARRAY_SIZE(ad), NULL, 0);
+        bt_le_ext_adv_stop(adv_set);
+        //LOG_HEXDUMP_INF(adv_payload, 10, "First 10 Bytes of Payload");
+        err = bt_le_ext_adv_set_data(adv_set, ad, ARRAY_SIZE(ad), NULL, 0);
+        if (err) {
+            printk("Failed to update extended adv data (err %d)\n", err);
+            return;
+        }
+        err = bt_le_ext_adv_start(adv_set, BT_LE_EXT_ADV_START_DEFAULT);
+        if (err) {
+            printk("Failed to start extended advertising (err %d)\n", err);
+            return;
+        }
+
         k_sleep(K_MSEC(SENSOR_ADV_INTERVAL_MS));
     }
 }
